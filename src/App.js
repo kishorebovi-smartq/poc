9cf8dc23043159d0a7de4a20307bc9d3006e44fe
Mayank Manu <mayankmanu@INSQLQ6TG7R9XJQ.local>
2024-01-12 14:52:55 +0530

    material ui installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
-import Login from './components/login';
-import { Provider } from 'react-redux'
-import store from './store'
+import "./App.css";
+import Login from "./components/login";
+import { Provider } from "react-redux";
+import store from "./store";
 function App() {
-
   return (
-    <Provider store={store} >
-      <Login test={'hello'} />
+    <Provider store={store}>
+      <Login test={"hello"} />
     </Provider>
   );
 }
